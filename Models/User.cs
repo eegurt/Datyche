668e50e21b1891100ec3dc619157a13066099aac
eegurt <alihan28.06.0450@gmail.com>
2022-07-29 21:49:42 +0500

    password validation

--- Models/User.cs
+++ Models/User.cs
@@ -12,11 +12,12 @@ namespace Datyche.Models
 
         [Required]
         [StringLength(20, MinimumLength = 4)]
-        [RegularExpression(@"[a-zA-Z]+[\w]*")]
+        [RegularExpression(@"[a-zA-Z]{1}[\w]*")] // starts with letter, includes any word character [a-zA-Z0-9_]
         public string? Username { get; set; }
 
         [Required]
         [StringLength(20, MinimumLength = 8)]
+        [RegularExpression(@"(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\S]+")] // at least 1 lowercase letter, 1 uppercase letter, 1 digit, 1 or more of any non-space characters
         public string? Password { get; set; }
 
         public User() {}
