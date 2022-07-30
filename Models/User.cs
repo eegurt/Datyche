df3c6270b5bed2b463e02faafd2cffaf62bc35b6
eegurt <alihan28.06.0450@gmail.com>
2022-07-30 17:58:13 +0500

    email validation + error messages

--- Models/User.cs
+++ Models/User.cs
@@ -7,19 +7,23 @@ namespace Datyche.Models
     {
         public ObjectId Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Обязательно к заполнению")]
+        [StringLength(50)]
+        [EmailAddress(ErrorMessage = "Некорректный адрес электронной почты")]
         public string? Email { get; set; }
 
-        [Required]
-        [StringLength(20, MinimumLength = 4)]
-        [RegularExpression(@"[a-zA-Z]{1}[\w]*")] // starts with letter, includes any word character [a-zA-Z0-9_]
+        [Required(ErrorMessage = "Обязательно к заполнению")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Никнейм должен иметь от 4 до 20 символов включительно")]
+        // starts with letter, includes any word characters
+        [RegularExpression(@"[a-zA-Z]{1}[\w]*", ErrorMessage = "Никнейм должен начинаться с буквы. Допустимые символы: буквы, цифры, нижнее подчеркивание")]
         public string? Username { get; set; }
 
-        [Required]
-        [StringLength(20, MinimumLength = 8)]
-        [RegularExpression(@"(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\S]+")] // at least 1 lowercase letter, 1 uppercase letter, 1 digit, 1 or more of any non-space characters
+        [Required(ErrorMessage = "Обязательно к заполнению")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Пароль должен иметь от 8 до 20 символов включительно")]
+        // at least 1 lowercase letter, 1 uppercase letter, 1 digit, 1 or more of any non-space characters
+        [RegularExpression(@"(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\S]+", ErrorMessage = "Пароль должен содержать как минимум 1 строчную, 1 заглавную буквы и 1 цифру.")]
         public string? Password { get; set; }
 
-        public User() {}
+        public User() { }
     }
 }
